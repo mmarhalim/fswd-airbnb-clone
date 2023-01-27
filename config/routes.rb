b5395b4aa50a167b45d3bff20c63272cf3cdb6fa
mmarhalim <115380281+mmarhalim@users.noreply.github.com>
2023-01-27 13:35:00 -0700

    bookings api

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+  get '/property/:id' => 'static_pages#property'
+
 
   namespace :api do
     # Add routes below this line
     resources :users, only: [:create]
     resources :sessions, only: %i[create destroy]
     resources :properties, only: [:index, :show]
-
-
+    resources :bookings, only: [:create]
+    
+    get '/properties/:id/bookings' => 'bookings#get_property_bookings'
     get '/authenticated' => 'sessions#authenticated'
+
   end
 end
